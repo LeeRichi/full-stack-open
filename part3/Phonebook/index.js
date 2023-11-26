bd3db87e807f64abeb868148f3538bae7da51f2c
Rich <applerich0306@gmail.com>
2023-11-26 13:52:58 +0200

    3.11

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -10,24 +10,24 @@ var morgan = require('morgan')
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
@@ -90,10 +90,6 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-// app.get('*', (req, res) => {
-//   res.sendFile('index.html', { root: 'dist' });
-// });
-
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
@@ -130,7 +126,6 @@ app.get('/info', (request, response) =>
   `);
 })
 
-// const PORT = 3001
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
